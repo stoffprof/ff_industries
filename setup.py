32baeca20958e25e8df2d08a49407f757ed0733e
Noah Stoffman <stoffman@gmail.com>
2025-02-13 20:09:38 -0500

    Fix package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_package",
+    name="ff_industries",
     version="0.25.1",
     packages=find_packages(),
     install_requires=[
